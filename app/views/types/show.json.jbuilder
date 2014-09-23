a7510a792d738cfa68627435053b34c48a232b70
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-09-23 20:10:07 +0200

    Closes #5 - Create Type model

---
+++ app/views/types/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @type, :id, :title, :created_at, :updated_at
---
+++ db/migrate/20140923180857_create_types.rb
@@ -0,0 +1,9 @@
+class CreateTypes < ActiveRecord::Migration
+  def change
+    create_table :types do |t|
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
