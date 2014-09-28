c5e191f9f477ee9ec79a2b35113673b9d44a796f
Sveta Krasikova <contact@svetakrasikova.eu>
2014-09-28 17:09:12 +0200

    Adds validations to the car attributes

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,11 @@
 class Car < ActiveRecord::Base
+	validates :brand, presence: true
+	validates :type, presence: true
+	validates :color, presence: true
+	validates :age, presence: true
+	validates :mileage, presence: true
+	validates :title, presence: true
+	
   belongs_to :user
   belongs_to :brand
   belongs_to :type
