a852ebb257a334517a60f10daceb182ed8a29642
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-09-28 15:41:30 +0200

    fix little copy and paste bug in ApplicationHelper for loading gravatar images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def avatar_url(user)
-      default_url = "#{root_url}images/guest.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
   end
 end
